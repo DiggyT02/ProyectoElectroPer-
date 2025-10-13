51413b2489959c5c2f04a78d3e479527c7aa5c34
Diggy Felix <diggytonyfelixtipacti@gmail.com>
2025-10-13 12:41:00 -0500

    Ultimo avance en clase

--- controllers/personaController.js
+++ controllers/personaController.js
@@ -13,8 +13,32 @@ const obtenerTodas = async (req,res) => {
   }
 }
 
+
+//req => require (petici+n/solicitud)
+//req.params  : parámetro URL
+//req.body    : parámetro JSON
+//req.file    : Envía un archivo binario
 const crear = async (req, res) => {
-  
+  try{
+      //1. Recibir los datos del formulario(texto)
+      const {apellidos, nombres, dni, telefono } = req.body
+      //2.Recibir la fotografía
+      const fotografia = req.file ? `/uploads/${req.file.filename}` : null;
+
+      //3. Validación
+
+      //4. Guardar nuevo registro
+      const [result] = await db.query("INSERT INTO personas (apellidos, nombres, dni, telefono, fotografia) VALUES  (?,?,?,?,?)",
+        [apellidos, nombres, dni, telefono, fotografia])
+
+        res.status(201).json({
+          id: result.insertId,
+          message: 'Registro correcto'
+        })
+  }catch (e){
+    console.error(e)
+
+  }
 }
 
 const actualizar = async (req, res) => {
